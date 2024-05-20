3cdb94e7ca996a24e11c852eca71a2064dc92bf2
hungvietle227 <leviethung220703@gmail.com>
2024-05-21 00:49:58 +0700

    [Add]_Project

---
+++ Hunglvse171595_NET1701_A01/LeVietHungWPF/AdminScreen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LeVietHungWPF
+{
+    /// <summary>
+    /// Interaction logic for AdminScreen.xaml
+    /// </summary>
+    public partial class AdminScreen : Window
+    {
+        public AdminScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ManageCustomer customerForm = new ManageCustomer();
+            main.Content = customerForm;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            ManageRoom roomForm = new ManageRoom();
+            main.Content = roomForm;
+        }
+    }
+}
